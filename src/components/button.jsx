3c828bc62ae7f94df9ddd9c0caf0fe78cdc54ba5
krishangoyal12 <krishangoyal717@gmail.com>
2025-03-12 01:07:16 +0530

    Completed the assignment

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,9 @@
-// write button card here
+import '../components/button.css'
+
+export default function button() {
+  return (
+    <>
+        <button className='button'>View Product</button>
+    </>
+  )
+}
--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,25 @@
-// write product card here
+import "../components/productcard.css";
+import Button from "./button";
+
+export default function productcard() {
+  const product_image =
+    "https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8bGFwdG9wfGVufDB8fDB8fHww";
+
+  const product_name = "ASUS TUF Gaming A15, AMD Ryzen 7";
+
+  const price = "63990";
+  return (
+    <>
+      <div className="product-card">
+        <img src={product_image} alt="Product-image" className="product-img" />
+        <h3>{product_name}</h3>
+        <h2>
+          {" "}
+          <b>Price: </b>
+          {price}
+        </h2>
+        <Button/>
+      </div>
+    </>
+  );
+}
